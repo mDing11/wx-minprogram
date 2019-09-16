2d47c26398c17d048b6af493cc65176c43ed5312
ding.mei <ding.mei@eisoo.com>
2019-09-16 18:43:02 +0800

    微信小程序搭建

---
+++ miniprogram/pages/shareBook/bookItem/BookItem.js
@@ -0,0 +1,35 @@
+// pages/shareBook/bookItem/BookItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name:{
+      type:String,
+      value:'案例学web教程'
+    },
+    score:{
+      type:String,
+      value:'50'
+    },
+    src:{
+      type:String,
+      value:'../../../images/share-book2.png'
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ miniprogram/pages/shareBook/shareBook.js
@@ -0,0 +1,15 @@
+Page({
+
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+    //获得dialog组件
+    this.dialog = this.selectComponent("#dialog");
+  },
+
+  data: {
+    
+  },
+
+})
